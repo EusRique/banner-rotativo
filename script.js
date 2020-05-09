8fdfb1c4ba310621f5f06d214070744077aac850
Henrique <hiquesc_@hotmail.com>
2020-05-09 12:00:31 -0300

    feat: script js

---
+++ script.js
@@ -0,0 +1,24 @@
+let time = 2000
+let currentImageIndex = 0
+let images = document.querySelectorAll("#slide img")
+let max = images.length
+
+function nextImage() {
+  images[currentImageIndex].classList.remove("selected")
+
+  currentImageIndex ++
+
+  if(currentImageIndex >= max)
+    currentImageIndex = 0
+
+  images[currentImageIndex].classList.add("selected")
+
+}
+
+function start() {
+  setInterval(() => {
+    nextImage()
+  }, time)
+}
+
+window.addEventListener("load", start)
